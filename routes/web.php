7b5eb91d9613aeeae57556c2013aee283880c4d4
chantyTUN <cambodai4khmeru@gmail.com>
2023-05-17 01:08:14 +0700

    14 - forgot and reset password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,8 +9,8 @@ use App\Http\Controllers\Demo\DemoController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -18,20 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/about-us',[DemoController::class, 'index']);
-// Route::get('/contact',[DemoController::class, 'contact']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 Route::controller(DemoController::class)->group(function (){
-    Route::get('/about-usasdasdasd','index')->name('page.about');
+    Route::get('/about','index')->name('page.about');
     Route::get('/contact','contact')->name('page.contact');
 });
 
-// Route::get('/about-us', function () {
-//    return view('about-us');
-// });
 
-// Route::get('/contact', function () {
-//    return view('contact');
-// });
-
-
+require __DIR__.'/auth.php';
