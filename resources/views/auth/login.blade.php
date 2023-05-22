c7a06a9b41c65059ca3a770ba0153f262fd823af
chantyTUN <cambodai4khmeru@gmail.com>
2023-05-23 01:45:01 +0700

    23 && 24 - Forgot Password Customize Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                             <label class="form-check-label">
                                 <input type="checkbox" class="form-check-input"> Remember me </label>
                             </div>
-                            <a href="#" class="forgot-pass">Forgot password</a>
+                            <a href="{{ url('forgot-password') }}" class="forgot-pass">Forgot password</a>
                         </div>
                         <div class="text-center">
                             <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
