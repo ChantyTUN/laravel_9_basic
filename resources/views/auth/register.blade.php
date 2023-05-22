76c99b062329fa5614bd4046700cfffb68a52cbe
chantyTUN <cambodai4khmeru@gmail.com>
2023-05-23 00:51:49 +0700

    21 - Register Customize Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,58 +1,123 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Register</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="row w-100 m-0">
+          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+            <div class="card col-lg-4 mx-auto">
+              <div class="card-body px-5 py-5">
+                <h3 class="card-title text-left mb-3">Register</h3>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control p_input" required autofocus autocomplete="name" placeholder="Name" >
+                            @if($errors->has('name'))
+                                <span class="text-danger">
+                                    {{ $errors->first('name') }}
+                                </span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" class="form-control p_input" value="{{ old('username') }}" required autocomplete="username"  placeholder="Username" >
+                            @if($errors->has('username'))
+                                <span class="text-danger">
+                                    {{ $errors->first('username') }}
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="form-control p_input" required autocomplete="email"  placeholder="Email" >
+                            @if($errors->has('email'))
+                                <span class="text-danger">
+                                    {{ $errors->first('email') }}
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control p_input" required autocomplete="password">
+                            @if($errors->has('password'))
+                                <span class="text-danger">
+                                    {{ $errors->first('password') }}
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Password Confirmation</label>
+                            <input type="password" name="password_confirmation" class="form-control p_input" required autocomplete="password_confirmation" >
+                            @if($errors->has('password_confirmation'))
+                                <span class="text-danger">
+                                    {{ $errors->first('password_confirmation') }}
+                                </span>
+                            @endif
+                        </div>
+                        <div class="form-group d-flex align-items-center justify-content-between">
+                            <div class="form-check">
+                            <label class="form-check-label">
+                                <input type="checkbox" class="form-check-input"> Remember me </label>
+                            </div>
+                            <a href="#" class="forgot-pass">Forgot password</a>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                        </div>
+                        <div class="d-flex">
+                            <button class="btn btn-facebook col mr-2">
+                            <i class="mdi mdi-facebook"></i> Facebook </button>
+                            <button class="btn btn-google col">
+                            <i class="mdi mdi-google-plus"></i> Google plus </button>
+                        </div>
+                        <p class="sign-up text-center">Already have an Account?<a href="#"> Sign Up</a></p>
+                        <p class="terms">By creating an account you are accepting our<a href="#"> Terms & Conditions</a></p>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
         </div>
-        <!-- Username -->
-        <div class="mt-4">
-            <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <!-- row ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('backend/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('backend/assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/misc.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/settings.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/todolist.js') }}"></script>
+    <!-- endinject -->
+  </body>
+</html>
