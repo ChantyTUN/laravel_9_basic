2873a4beaf07b5b3dbb53908846edae37d071841
chantyTUN <cambodai4khmeru@gmail.com>
2023-05-25 01:01:05 +0700

    27 - Admin Profile Jquery Change Image src

--- resources/views/admin/edit_profile.blade.php
+++ resources/views/admin/edit_profile.blade.php
@@ -1,5 +1,8 @@
 @extends('admin.admin_master')
 @section('admin')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
 <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
@@ -20,21 +23,15 @@
                             </div>
                             <div class="form-group">
                                 <label>File upload</label>
-                                <input type="file" name="img[]" class="file-upload-default">
                                 <div class="input-group col-xs-12">
-                                <input type="text" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
-                                <span class="input-group-append">
-                                    <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
-                                </span>
+                                    <input type="file" name="profile_image" id="image_profile" class="form-control file-upload-info" placeholder="Upload Image" accept="image/*" >
                                 </div>
                             </div>
                             <div class="form-group">
-                            <div class="profile-pic">
+                                <div class="profile-pic">
                                     <div class="count-indicator">
-                                    <img class="img-ms rounded-circle " src="{{ asset('backend/assets/images/faces/face15.jpg') }}" alt="">
+                                        <img class="img-ms rounded-circle " id="showImageProfile" src="{{ asset('backend/assets/images/faces/face15.jpg') }}" alt="">
                                     <span class="count bg-success"></span>
-                                    </div>
-                                    <div class="profile-name">
                                     </div>
                                 </div>
                             </div>
@@ -45,5 +42,16 @@
                   </div>
                 </div>
               </div>
-
+<!-- Click Change Image  -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#image_profile").change(function(e){
+           var readerView = new FileReader();
+           readerView.onload = function(e){
+            $("#showImageProfile").attr("src",e.target.result);
+           }
+           readerView.readAsDataURL(e.target.files['0'])
+        });
+    });
+</script>
 @endsection
