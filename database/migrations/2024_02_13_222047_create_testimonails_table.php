f10a97ba5418a4599ed56628297d54cb25f75466
ChantyTUN <cambodia4khmeru@gmail.com>
2024-02-14 05:27:25 +0700

    61-backend Testimonail

---
+++ database/migrations/2024_02_13_222047_create_testimonails_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonails', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rate');
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->string('position')->nullable();
+            $table->text('dec');
+            $table->string('status')->default(1);
+            $table->softDeletes();
+            $table->timestamps();     
+            $table->unsignedInteger('deleted_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedInteger('created_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonails');
+    }
+};
